69912b61f1f7dafc51375cf4acf1d1d9d1ada1da
Chris Churas <churas@gmail.com>
2017-07-03 15:30:25 -0700

    added singularity

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,11 +11,25 @@ yum install -y opencv opencv-python numpy numpy-f2py scipy python-pip
 updatedb
 
 pip install scikit-learn
+pip install chmutil
 
 # imod
 echo "Imod installation"
-wget http://bio3d.colorado.edu/imod/AMD64-RHEL5/imod_4.7.15_RHEL6-64_CUDA6.0.csh
-chmod a+x imod_4.7.15_RHEL6-64_CUDA6.0.csh
-./imod_4.7.15_RHEL6-64_CUDA6.0.csh -yes
+wget http://bio3d.colorado.edu/imod/AMD64-RHEL5/imod_4.9.4_RHEL6-64_CUDA6.0.sh
+chmod a+x imod_4.9.4_RHEL6-64_CUDA6.0.sh
+./imod_4.9.4_RHEL6-64_CUDA6.0.sh -yes
+rm -f imod*sh
 
+# singularity install
+
+VERSION=2.3.1
+wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+tar xvf singularity-$VERSION.tar.gz
+cd singularity-$VERSION
+./configure --prefix=/usr/local
+make
+make install
+cd ..
+rm -f singularity-$VERSION.tar.gz
+rm -rf singularity-$VERSION
 
