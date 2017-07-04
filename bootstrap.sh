fd8a9b62072f1fa38f23ff48c024d6e5b2d7ffde
Chris Churas <churas@gmail.com>
2017-07-03 17:18:02 -0700

    switched to using puppet-nbcrtrain puppet configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,26 @@
 #!/usr/bin/env bash
 
-echo "Installing base packages"
-yum install -y cmake git unzip tree 
-yum install -y epel-release
-yum install -y python python-devel qt qt-devel wget tcsh xauth xclock
-yum install -y gcc-c++ mlocate time ImageMagick ImageMagick-c++
-yum install -y xorg-x11-fonts-*
-yum install -y mesa-*
-yum install -y opencv opencv-python numpy numpy-f2py scipy python-pip
-updatedb
+# echo "add config manager to disable puppet labs repo"
+# yum install yum-utils -y
 
-pip install scikit-learn
-pip install chmutil
+# echo "disable puppet labs repo cause there is a gpg key problem"
+# echo "preventing update from working"
+# yum-config-manager --disable puppetlabs-pc1
 
-# imod
-echo "Imod installation"
-wget http://bio3d.colorado.edu/imod/AMD64-RHEL5/imod_4.9.4_RHEL6-64_CUDA6.0.sh
-chmod a+x imod_4.9.4_RHEL6-64_CUDA6.0.sh
-./imod_4.9.4_RHEL6-64_CUDA6.0.sh -yes
-rm -f imod*sh
+# echo "Update all packages first"
+# yum update -y
 
-# singularity install
+echo "Installing git"
+yum install -y git
 
-VERSION=2.3.1
-wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
-tar xvf singularity-$VERSION.tar.gz
-cd singularity-$VERSION
-./configure --prefix=/usr/local
-make
-make install
-cd ..
-rm -f singularity-$VERSION.tar.gz
-rm -rf singularity-$VERSION
+echo "Cloning puppet-nbcrtrain"
+git clone https://github.com/CRBS/puppet-nbcrtrain.git nbcrtrain
 
+# append execution command cause I cannot for the life of
+# me get puppet to use the catalog 
+# TODO: fix this hack at some point
+
+echo -e "\n# Run the nbcrtrain open class\nclass { 'nbcrtrain::open': }\n" >> /home/vagrant/nbcrtrain/manifests/open.pp
+
+echo "Running puppet apply"
+/opt/puppetlabs/bin/puppet apply /home/vagrant/nbcrtrain/manifests/open.pp
